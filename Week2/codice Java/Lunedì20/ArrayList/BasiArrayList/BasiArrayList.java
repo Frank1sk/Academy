6ae08505910d69b8b8bbad78d5d073b6ff8fc7ba
Frank1sk <f.gionta90@gmail.com>
2024-05-20 23:14:23 +0200

    aggiunta cartella Lunedì20

---
+++ Week2/codice Java/Lunedì20/ArrayList/BasiArrayList/BasiArrayList.java
@@ -0,0 +1,18 @@
+package Lunedì20.ArrayList.BasiArrayList;
+import java.util.ArrayList;
+public class BasiArrayList {
+    public static void main(String[] args) {
+        
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo"); //aggiunge elemento all'AL
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        cars.set(0, "Opel"); //aggiunge elemento all'AL nella posizione 0
+        System.out.println(cars); //stampa l'AL
+        System.out.println(cars.size()); //stampa la dimensione dell'AL
+        System.out.println(cars.get(0)); //stampa l'elemento in posizione 0 dell'AL
+        cars.remove(0); //rimuove l'elemento in posizione 0 dell'AL
+        cars.clear(); //svuota l'AL
+    }
+}
